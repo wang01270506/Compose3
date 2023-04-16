c2f7f83fa2711bf10b4aad2d7104de96d7580cee
wang01270506 <s1100381@pu.edu.tw>
2023-04-16 22:00:55 +0800

    第二次commit

--- app/src/main/java/tw/edu/pu/csim/suuting/compose/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/suuting/compose/MainActivity.kt
@@ -11,7 +11,10 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.DragInteraction
+import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,7 +29,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -45,6 +50,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import tw.edu.pu.csim.suuting.compose.ui.theme.ComposeTheme
+import tw.edu.pu.csim.suuting.compose.ui.theme.Pink40
+import tw.edu.pu.csim.suuting.compose.ui.theme.Pink80
+import tw.edu.pu.csim.suuting.compose.ui.theme.PurpleGrey40
+import tw.edu.pu.csim.suuting.compose.ui.theme.PurpleGrey80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -87,7 +96,7 @@ fun Greeting(name: String) {
 
     Column{
         Row {
-            Text(text = "$name",color= Color.Yellow, fontSize = 36.sp,
+            Text(text = "$name",color= Color.Gray, fontSize = 36.sp,
                 fontFamily = FontFamily(Font(R.font.kai)))
 
             Image(
@@ -96,19 +105,50 @@ fun Greeting(name: String) {
             )
         }
 
-        Text(text = "作者:資管系-王思婷,年齡:20,水瓶座")
-        Text(text = "名言佳句:")
-        Text(text = "人生就是生活的過程。哪能沒有風沒有雨？正是因為有了風雨的洗禮才能看見斑斕的彩虹；有了失敗的痛苦才會嘗到成功的喜悅。")
+        Text(text = "作者:資管系-王思婷,年齡:20,水瓶座", color = PurpleGrey80)
+        Text(text = "名言佳句:", color = Pink80)
+        Text(text = "人生就是生活的過程。哪能沒有風沒有雨？正是因為有了風雨的洗禮才能看見斑斕的彩虹；有了失敗的痛苦才會嘗到成功的喜悅。", color = Pink80)
 
         val click_text = remember { mutableStateOf("") }
         val interactionSource = remember { MutableInteractionSource() }
+        val interactions = remember { mutableStateListOf<Interaction>() }
         val isPressed by interactionSource.collectIsPressedAsState()
+        val isPressedOrDragged = interactions.isNotEmpty()
+
         Button(
             onClick = { println("點擊了按鈕") },
             interactionSource = interactionSource) {
             Text("按我!，自我介紹")
         }
-
+        LaunchedEffect(interactionSource) {
+            interactionSource.interactions.collect { interaction ->
+                when (interaction) {
+                    is PressInteraction.Press -> {
+                        interactions.add(interaction)
+                    }
+                    is PressInteraction.Release -> {
+                        interactions.remove(interaction.press)
+                    }
+                    is PressInteraction.Cancel -> {
+                        interactions.remove(interaction.press)
+                    }
+                    is DragInteraction.Start -> {
+                        interactions.add(interaction)
+                    }
+                    is DragInteraction.Stop -> {
+                        interactions.remove(interaction.start)
+                    }
+                    is DragInteraction.Cancel -> {
+                        interactions.remove(interaction.start)
+                    }
+                }
+            }
+        }
+        val lastInteraction = when (interactions.lastOrNull()) {
+            is DragInteraction.Start -> "Dragged"
+            is PressInteraction.Press -> "Pressed"
+            else -> "No state"
+        }
         }
     }
 
